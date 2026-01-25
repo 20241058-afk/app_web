ba38692a6798ebb30a226cf8bee3698e3b455052
20241058-afk <20241058@uthh.edu.mx>
2026-01-24 22:10:42 -0600

    cooordenadas estaticas

--- geolocalizacion2/main.js
+++ geolocalizacion2/main.js
@@ -9,24 +9,26 @@ if(navigator.geolocation){
 
             const coordenadas=[lat,lon]
 
-            let map = L.map('map').setView(coordenadas, 13);
+            //let map = L.map('map').setView(coordenadas, 19);
+            let map = L.map('map').setView([21.171340, -98.432121], 19);
 
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 22,
                 attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(map);
 
-            let marcador = L.marker(coordenadas).addTo(map);
-            marcador.bindPopup('<b>Estoy aqui...</b><br>Mis corrdenadas son : <br>latitud : '+lat+'<br>longitud : '+lon).openPopup()
+            let marcador = L.marker([21.171340, -98.432121]).addTo(map);
+            marcador.bindPopup('<b>esta es es la ubicacion de mi casa.....</b><br>Mis corrdenadas son : <br>latitud : '+lat+'<br>longitud : '+lon).openPopup()
 
             var polygon = L.polygon([
                 [21.171405, -98.432113],
-                [21.171358, -98.432011],
                 [21.171358, -98.432011],
                 [21.171299, -98.432079],
                 [21.171274, -98.432106],
                 [21.171332, -98.432191]
             ]).addTo(map);
+
+            polygon.bindPopup("pertimetro de mi casa exacto").openPopup();
             //alert(lat+" , "+lon)
 x
         },
